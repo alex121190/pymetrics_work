e5270aac52c3d14075ead8ab2e3f7192a087c445
alex121190 <aliaksei.lapkouski@gmail.com>
2020-04-28 09:27:36 -0400

    New code for a filter

--- CP filter.py
+++ CP filter.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from time import sleep
 
 # init driver
 driver = webdriver.Chrome()
+driver.maximize_window()
+# driver.implicitly_wait(10)
 
 # open the url
-driver.get('https://www.staging.pymetrics.com/c/p')
+driver.get('https://www.staging.pymetrics.com/c/p/candidates')
 
 # Log in
 login = driver.find_element(By.XPATH, '//input[@id ="username"]')
@@ -17,6 +20,29 @@ password.clear()
 password.send_keys('6428531_Vbycr')
 
 driver.find_element(By.XPATH, '//button[@name="login"]').click()
+sleep(5)
 
 
-# driver.quit()
+# Go to candidates page
+# driver.find_element(By.XPATH, '//a[@title="Candidates"]').click()
+
+# Click Filters button
+filter_button = driver.find_element(By.XPATH, '//button[@class="QV-QATz9F5xIOwsy25LC0 "]')
+filter_button.click()
+
+
+# Click Invited checkbox
+driver.find_element(By.XPATH, '//div[@data-for="Invited"]//input[@type="checkbox"]').click()
+
+
+# Click Apply filter
+driver.find_element(By.XPATH, '//div[@class="_2YmJUj2HMf2xUn8JxXNUO_"]/button[@name="applyFilter"]').click()
+sleep(3)
+
+
+# # Should be 25 Invited
+invited = driver.find_elements(By.CSS_SELECTOR, '._1yU0N8HRUChgPbo5OC6WAL ')
+assert len(invited) == 25, f'Expected 25, but got {len(invited)}'
+# print(len(invited))
+
+driver.quit()
