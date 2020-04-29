49c91714cb3b19a154530a27366dcf79bb76dd40
alex121190 <aliaksei.lapkouski@gmail.com>
2020-04-29 08:27:12 -0400

    Added a loop

--- Client portal scripts/CP filter.py
+++ Client portal scripts/CP filter.py
@@ -30,80 +30,101 @@ sleep(5)
 filter_button = driver.find_element(By.XPATH, '//button[@class="QV-QATz9F5xIOwsy25LC0 "]')
 filter_button.click()
 
+checkboxes = driver.find_elements(By.CSS_SELECTOR, '._2dWgaQKyo0IbW6qgWYL79d:first-child > ._3tKwFY9SSNHJkaLR19S-JQ + div '
+                                                 '> div[data-for] input[type=checkbox]')
 
-# Click Invited checkbox
-driver.find_element(By.XPATH, '//div[@data-for="Invited"]//input[@type="checkbox"]').click()
-
-
-# Click Apply filter
-driver.find_element(By.XPATH, '//div[@class="_2YmJUj2HMf2xUn8JxXNUO_"]/button[@name="applyFilter"]').click()
-sleep(3)
-
-
-# # Should be 25 Invited
-invited = driver.find_elements(By.CSS_SELECTOR, '._1yU0N8HRUChgPbo5OC6WAL ')
-assert len(invited) == 25, f'Expected 25, but got {len(invited)}'
-# print(len(invited))
-
-
-# Click Filters button
-filter_button = driver.find_element(By.XPATH, '//button[@class="QV-QATz9F5xIOwsy25LC0 "]')
-filter_button.click()
-
-# Click Invited checkbox
-driver.find_element(By.XPATH, '//div[@data-for="Invited"]//input[@type="checkbox"]').click()
-
-# Click Registered checkbox
-driver.find_element(By.XPATH, '//div[@data-for="Registered"]//input[@type="checkbox"]').click()
-
-# Click Apply filter
-driver.find_element(By.XPATH, '//div[@class="_2YmJUj2HMf2xUn8JxXNUO_"]/button[@name="applyFilter"]').click()
-sleep(3)
+index = 0
+while index < len(checkboxes):
+    checkbox = checkboxes[index]
+    checkbox.click()
+    driver.find_element(By.XPATH, '//div[@class="_2YmJUj2HMf2xUn8JxXNUO_"]/button[@name="applyFilter"]').click()
+    sleep(3)
+    status = driver.find_elements(By.CSS_SELECTOR, '._1yU0N8HRUChgPbo5OC6WAL ')
+    # to do a loop for statuses
+    assert len(status) == 25, f'Expected 25, but got {len(status)}'
+    filter_button.click()
+    sleep(5)
+    checkboxes = driver.find_elements(By.CSS_SELECTOR,
+                                      '._2dWgaQKyo0IbW6qgWYL79d:first-child > ._3tKwFY9SSNHJkaLR19S-JQ + div '
+                                      '> div[data-for] input[type=checkbox]')
+    checkbox = checkboxes[index]
+    checkbox.click()
+    index += 1
 
 
-# # Should be 25 Invited
-registered = driver.find_elements(By.CSS_SELECTOR, '._1yU0N8HRUChgPbo5OC6WAL ')
-assert len(registered) == 25, f'Expected 25, but got {len(registered)}'
-# print(len(invited))
-
-# Click Filters button
-filter_button = driver.find_element(By.XPATH, '//button[@class="QV-QATz9F5xIOwsy25LC0 "]')
-filter_button.click()
-
-# Click Registered checkbox
-driver.find_element(By.XPATH, '//div[@data-for="Registered"]//input[@type="checkbox"]').click()
-
-# Click In progress checkbox
-driver.find_element(By.XPATH, '//div[@data-for="In Progress"]//input[@type="checkbox"]').click()
-
-# Click Apply filter
-driver.find_element(By.XPATH, '//div[@class="_2YmJUj2HMf2xUn8JxXNUO_"]/button[@name="applyFilter"]').click()
-sleep(3)
+# # Click Invited checkbox
+# driver.find_element(By.XPATH, '//div[@data-for="Invited"]//input[@type="checkbox"]').click()
+#
+#
+# # Click Apply filter
+# driver.find_element(By.XPATH, '//div[@class="_2YmJUj2HMf2xUn8JxXNUO_"]/button[@name="applyFilter"]').click()
+# sleep(3)
 
 
-# # Should be 25 Invited
-in_progress = driver.find_elements(By.CSS_SELECTOR, '._1yU0N8HRUChgPbo5OC6WAL ')
-assert len(in_progress) == 25, f'Expected 25, but got {len(in_progress)}'
-# print(len(invited))
-
-# Click Filters button
-filter_button = driver.find_element(By.XPATH, '//button[@class="QV-QATz9F5xIOwsy25LC0 "]')
-filter_button.click()
-
-# Click In progress checkbox
-driver.find_element(By.XPATH, '//div[@data-for="In Progress"]//input[@type="checkbox"]').click()
-
-# Click Completed checkbox
-driver.find_element(By.XPATH, '//div[@data-for="Completed"]//input[@type="checkbox"]').click()
-
-# Click Apply filter
-driver.find_element(By.XPATH, '//div[@class="_2YmJUj2HMf2xUn8JxXNUO_"]/button[@name="applyFilter"]').click()
-sleep(3)
-
-
-# # Should be 25 Invited
-completed = driver.find_elements(By.CSS_SELECTOR, '._1yU0N8HRUChgPbo5OC6WAL ')
-assert len(completed) == 25, f'Expected 25, but got {len(completed)}'
-# print(len(invited))
+# # # Should be 25 Invited
+# invited = driver.find_elements(By.CSS_SELECTOR, '._1yU0N8HRUChgPbo5OC6WAL ')
+# assert len(invited) == 25, f'Expected 25, but got {len(invited)}'
+# # print(len(invited))
+#
+#
+# # Click Filters button
+# filter_button = driver.find_element(By.XPATH, '//button[@class="QV-QATz9F5xIOwsy25LC0 "]')
+# filter_button.click()
+#
+# # Click Invited checkbox
+# driver.find_element(By.XPATH, '//div[@data-for="Invited"]//input[@type="checkbox"]').click()
+#
+# # Click Registered checkbox
+# driver.find_element(By.XPATH, '//div[@data-for="Registered"]//input[@type="checkbox"]').click()
+#
+# # Click Apply filter
+# driver.find_element(By.XPATH, '//div[@class="_2YmJUj2HMf2xUn8JxXNUO_"]/button[@name="applyFilter"]').click()
+# sleep(3)
+#
+#
+# # # Should be 25 Invited
+# registered = driver.find_elements(By.CSS_SELECTOR, '._1yU0N8HRUChgPbo5OC6WAL ')
+# assert len(registered) == 25, f'Expected 25, but got {len(registered)}'
+# # print(len(invited))
+#
+# # Click Filters button
+# filter_button = driver.find_element(By.XPATH, '//button[@class="QV-QATz9F5xIOwsy25LC0 "]')
+# filter_button.click()
+#
+# # Click Registered checkbox
+# driver.find_element(By.XPATH, '//div[@data-for="Registered"]//input[@type="checkbox"]').click()
+#
+# # Click In progress checkbox
+# driver.find_element(By.XPATH, '//div[@data-for="In Progress"]//input[@type="checkbox"]').click()
+#
+# # Click Apply filter
+# driver.find_element(By.XPATH, '//div[@class="_2YmJUj2HMf2xUn8JxXNUO_"]/button[@name="applyFilter"]').click()
+# sleep(3)
+#
+#
+# # # Should be 25 Invited
+# in_progress = driver.find_elements(By.CSS_SELECTOR, '._1yU0N8HRUChgPbo5OC6WAL ')
+# assert len(in_progress) == 25, f'Expected 25, but got {len(in_progress)}'
+# # print(len(invited))
+#
+# # Click Filters button
+# filter_button = driver.find_element(By.XPATH, '//button[@class="QV-QATz9F5xIOwsy25LC0 "]')
+# filter_button.click()
+#
+# # Click In progress checkbox
+# driver.find_element(By.XPATH, '//div[@data-for="In Progress"]//input[@type="checkbox"]').click()
+#
+# # Click Completed checkbox
+# driver.find_element(By.XPATH, '//div[@data-for="Completed"]//input[@type="checkbox"]').click()
+#
+# # Click Apply filter
+# driver.find_element(By.XPATH, '//div[@class="_2YmJUj2HMf2xUn8JxXNUO_"]/button[@name="applyFilter"]').click()
+# sleep(3)
+#
+#
+# # # Should be 25 Invited
+# completed = driver.find_elements(By.CSS_SELECTOR, '._1yU0N8HRUChgPbo5OC6WAL ')
+# assert len(completed) == 25, f'Expected 25, but got {len(completed)}'
+# # print(len(invited))
 
 driver.quit()
