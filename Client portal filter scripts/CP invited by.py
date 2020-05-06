04bd277587438220ca41ec89899161537546ed50
alex121190 <alex121190@gmail.com>
2020-05-06 16:16:22 -0400

    New files and names

---
+++ Client portal filter scripts/CP invited by.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+# init driver
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+# open the url
+driver.get('https://www.staging.pymetrics.com/c/p/candidates')
+
+# Log in
+login = driver.find_element(By.XPATH, '//input[@id ="username"]')
+login.clear()
+login.send_keys('alex.lapkouski@pymetrics.com')
+
+password = driver.find_element(By.XPATH, '//input[@id ="password"]')
+password.clear()
+password.send_keys('6428531_Vbycr')
+
+driver.find_element(By.XPATH, '//button[@name="login"]').click()
+sleep(5)
+
+# Click Filters button
+filter_button = driver.find_element(By.XPATH, '//button[@class="QV-QATz9F5xIOwsy25LC0 "]')
+filter_button.click()
