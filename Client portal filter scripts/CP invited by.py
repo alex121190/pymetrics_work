7069eb4480cba3ec4991ee0162ca1ceaccd5d811
alex121190 <alex121190@gmail.com>
2020-05-12 13:40:11 -0400

    Completed filter is added

--- Client portal filter scripts/CP invited by.py
+++ Client portal filter scripts/CP invited by.py
@@ -25,3 +25,24 @@ sleep(5)
 filter_button = driver.find_element(By.XPATH, '//button[@class="QV-QATz9F5xIOwsy25LC0 "]')
 filter_button.click()
 
+search_line = driver.find_element(By.CSS_SELECTOR, '[placeholder="Search for others"]')
+search_line.clear()
+search_line.send_keys('lapkouski')
+needed_person = driver.find_element(By.CSS_SELECTOR, '[data-for="Alex.Lapkouski@pymetrics.com"] input[type=checkbox]').click()
+apply_button = driver.find_element(By.XPATH, '//div[@class="_2YmJUj2HMf2xUn8JxXNUO_"]/button[@name="applyFilter"]').click()
+sleep(2)
+
+# Assertion
+emails_needed = driver.find_elements(By.XPATH, "//div[@class='_1yU0N8HRUChgPbo5OC6WAL ']/a[@class='_2KIninGfXsD6cdf3q8JMh "
+                                               "FkFyqEJe1OWSPqqHhdFNP _1vcpikaMB6B_c_j9hakYb6']//div[@style='text-align: left;']")
+
+if len(emails_needed) > 0:
+    for email in emails_needed:
+        if "alex" in email.text or "Alex" in email.text:
+            continue
+        else:
+            print("Wrong user")
+else:
+    print("No results")
+
+driver.quit()
